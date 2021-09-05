ffb15a22a341dfffe9494e1592f5552445b84ba9
OmAximani0 <aximaniom@gmail.com>
2021-09-05 21:03:18 +0530

    Deploy v4

--- backend/settings.py
+++ backend/settings.py
@@ -162,6 +162,8 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
